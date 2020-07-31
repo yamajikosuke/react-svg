70a632742dab1d789f6b47cec0455b03eeb17295
yamaji <info@ky2000.moo.jp>
2020-08-01 02:00:04 +0900

    update

--- src/components/ClickRect.tsx
+++ src/components/ClickRect.tsx
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-interface elementColor {
-  [key: string]: string;
+interface elementColorProp {
+  [key: string]: { color: string; isComplete: boolean };
 }
 
 export const ClickRect: React.FC = (): JSX.Element => {
-  // set color
-  const color: elementColor = { p1: "orange", p2: "gray" };
+  const [pickColor, setPickColor] = useState<string>("red");
+  const [isComplete, setIsComplete] = useState<boolean>(false);
+
+  let elementColor: elementColorProp[] = [
+    { p1: { color: "orange", isComplete: false } },
+    { p2: { color: "gray", isComplete: false } },
+    { p3: { color: "red", isComplete: false } },
+  ];
+
   React.useEffect(() => {
     const parts = document.getElementsByClassName("parts");
     const elements = Array.from(parts); // collection を arrayに変換
-    elements.map((element: Element) => {
+    elements.forEach((element: Element) => {
       let id = element.getAttribute("id");
       document.querySelector(`#${id}`)?.addEventListener("click", () => {
-        if (id) {
-          element.setAttribute("fill", color[id]);
-        }
+        elementColor.forEach((elm) => {
+          if (id && id === Object.keys(elm)[0]) {
+            //  if (pickColor === elm[id]["color"]) {
+            element.setAttribute("fill", elm[id]["color"]);
+            elm[id]["isComplete"] = true;
+            //    }
+          }
+        });
+        console.log(checkIsComplete());
+        if (checkIsComplete() === elementColor.length) setIsComplete(true);
       });
     });
+    console.log(isComplete);
   });
+
+  const checkIsComplete = () => {
+    return elementColor.filter((element) => {
+      return element[Object.keys(element)[0]].isComplete === true;
+    }).length;
+  };
 
   return (
     <main>
-      <svg width="200" height="200" className="svgcode">
+      <svg width="300" height="300" className="svgcode">
         <g>
           <rect
             className="parts"
@@ -39,8 +60,17 @@ export const ClickRect: React.FC = (): JSX.Element => {
             x="100"
             y="70"
             width="100"
-            height="170"
+            height="150"
             fill="black"
+          ></rect>
+          <rect
+            id="p3"
+            className="parts"
+            x="70"
+            y="130"
+            width="100"
+            height="150"
+            fill="blue"
           ></rect>
         </g>
       </svg>
